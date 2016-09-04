70fa1fdfac273e16d162b3bf6b3c6a64edfdd466
Derric Atzrott <zellfaze@zellfaze.org>
2016-09-04 12:53:33 -0400

    Updated resources file
    It now contains the standard AGPL header and it has also been
    modified to add a little bit more structure to how the resources
    are arranged in the file. This should allow for easier reading
    by humans.

--- application/third_party/resources.php
+++ application/third_party/resources.php
@@ -1,25 +1,55 @@
 <?php
+/*
+Copyright (c) 2016 "Derric Atzrott"
+
+This file is part of Star-Game.
+
+Star-Game is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 class Resources {
   public static function getList() {
     return array(
-      'metals',       //Used as general building materials
-      'fuel',         //Used to power slower than light speed ships and terestrial vehicles
-      'food',         //Used to feed population
-      'population',   //Literally people
-      'science',      //Used to purchase technologies and create inventions
-      'antimatter',   //Used to power anti-matter drives and weapons
-      'deuterium',    //Used to power slower than light speed ships
-      'he3',          //Fissionable energy source, used for ships and power reactors
-      'monopoles',    //TODO: Not sure what these might be used for yet
-      'polymers',     //Used as general building materials
-      'rare_earth',   //Used to build electronics
-      'oxygen',       //Used for breating also required for fuel to work
-      'hydrogen',     //Used as fuel
-      'lithium',      //Used for electrical storage
-      'gluon_arrays', //Very expensive to produce, used for gravitational astronomy and some FTL designs
-      'collapsium',   //Extremely durable metal used for armor
-      'dilithium',    //Very rare, used to power some FTL designs
+      //Required
+      'food',         //Used to feed population, common
+      'population',   //Literally people, common
+      'science',      //Used to purchase technologies and create inventions, common
+      'oxygen',       //Used for breating also required for fuel to work, common
+      'electricity',  //Used to power all the things, common
+      
+      //General Building Materials
+      'metals',       //Used as general building materials, common
+      'polymers',     //Used as general building materials, uncommon
+      'rare_earth',   //Used to build electronics, rare
+      
+      //Fuel Sources
+      'fuel',         //Used to power slower than light speed ships and terestrial vehicles, common
+      'antimatter',   //Used to power anti-matter drives and weapons, rare
+      'deuterium',    //Used to power slower than light speed ships, uncommon
+      'he3',          //Fissionable energy source, used for ships and power reactors, common
+      'uranium',      //Fissionable energy source, used for ships and power reactors, uncommon
+      'hydrogen',     //Used as fuel, common
+      
+      //FTL Materials
+      'gluon_arrays', //Very expensive to produce, used for gravitational astronomy and some FTL designs, rare
+      'dilithium',    //Very rare, used to power some FTL designs, very rare
+      
+      //Other
+      'monopoles',    //Used to produce levitating terestrial buildings and units, rare
+      'lithium',      //Used for electrical storage, common
+      'collapsium',   //Extremely durable metal used for armor, rare
     );
   }
 }
